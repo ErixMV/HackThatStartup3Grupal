da1709f6e0ea11a3f0a5a6095d07282f222fcf6e
Erix Mamani Villacresis <erixmv022@gmail.com>
2021-05-30 19:57:59 +0200

    Added cookie-parser

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ import routes from './routes';
 import xss from 'xss-clean';
 import morgan from 'morgan';
 import session from 'express-session';
+import cookieParser from 'cookie-parser'
 
 import { graphqlHTTP } from 'express-graphql';
 import schema from './graphql/schema';
@@ -21,13 +22,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(xss());
 app.use(morgan('dev'));
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(cookieParser("SECRET"));
 app.use(session({
     secret: "SECRET",
     resave: true,
     saveUninitialized: true
-}))
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Server API REST routes
 app.use('/api', routes);
